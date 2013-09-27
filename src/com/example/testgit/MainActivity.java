0f99c59235a1b55dbb6601d5b410a0331b1bebaf
Erik M Roll <ErikMRoll@gmail.com>
2013-09-27 08:30:41 -0400

    v2 source code

--- src/com/example/testgit/MainActivity.java
+++ src/com/example/testgit/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	//v2 source code
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
